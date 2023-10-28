18ad22b73ee50c020c925cd2464fd48179fb0058
VMM-MMV <mihai.vieru@isa.utm.md>
2023-10-28 06:05:04 +0300

    better status print

--- src/main/java/com/project/ngit/Commands/StatusCommand.java
+++ src/main/java/com/project/ngit/Commands/StatusCommand.java
@@ -13,33 +13,38 @@ public class StatusCommand {
         Map<String, FileStatus> serializedData = AddCommand.loadSerializedData(ngitPath.resolve("index/changes.ser"));
 
         for (Map.Entry<String, FileStatus> entry : serializedData.entrySet()) {
-            System.out.println(entry);
-        }
+            String filePath = entry.getKey();
+            FileStatus fileStatus = entry.getValue();
 
-//        for (Map.Entry<String, FileStatus> entry : serializedData.entrySet()) {
-//            String filePath = entry.getKey();
-//            FileStatus fileStatus = entry.getValue();
-//
-//            Path actualPath = Path.of(repositoryPath, filePath);
-//            if (Files.exists(actualPath)) {
-//                FileTime currentModifiedTime;
-//                try {
-//                    currentModifiedTime = Files.getLastModifiedTime(actualPath);
-//                } catch (IOException e) {
-//                    System.out.println("Error getting last modified time for: " + actualPath);
-//                    continue;
-//                }
-//
-//
-//
-//                if (fileStatus.initialTimestamp().equals(fileStatus.activeTimestamp())) {
-//                    System.out.println(actualPath + " is a new file.");
-//                } else if (!fileStatus.activeTimestamp().equals(currentModifiedTime.toString())) {
-//                    System.out.println(actualPath + " has been modified. Stored: " + fileStatus.activeTimestamp() + " Current: " + currentModifiedTime);
-//                }
-//            } else {
-//                System.out.println(actualPath + " no longer exists.");
-//            }
-//        }
+            Path actualPath = Path.of(filePath);
+            if (Files.exists(actualPath)) {
+                FileTime currentModifiedTime;
+                try {
+                    currentModifiedTime = Files.getLastModifiedTime(actualPath);
+                } catch (IOException e) {
+                    System.out.println("Error getting last modified time for: " + actualPath);
+                    continue;
+                }
+
+                String pathStr = actualPath.toString();
+                String[] parts = pathStr.split("UniversityGit");
+                String relativePath;
+                if (parts.length > 1) {
+                    relativePath = parts[1];
+                    System.out.println(relativePath);
+                } else {
+                    relativePath = parts[0];
+                }
+
+                if (fileStatus.initialTimestamp().equals(fileStatus.activeTimestamp())) {
+                    System.out.println(relativePath + " is a new file.");
+                } else if (!fileStatus.activeTimestamp().equals(currentModifiedTime.toString())) {
+                    System.out.println(relativePath + " has been modified. Stored: " + fileStatus.activeTimestamp() + " Current: " + currentModifiedTime);
+                }
+
+            } else {
+                System.out.println(actualPath + " no longer exists.");
+            }
+        }
     }
 }
